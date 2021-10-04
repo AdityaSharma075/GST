6e5e142ef94bf86b354c0476004a078d1ad77365
Aditya Sharma <aditya07543@gmail.com>
2021-10-04 10:26:48 +0530

    Add route to basic home page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// require express 
 const express = require ('express');
 const app = express();
 
+// port on which our server will run
 const port = 8000;
 
+app.use('/' , require('./routes'));
+
+// Make server listen to our port
 app.listen(port ,function(err){
     if(err){
         console.log(`error in loading the server :${port}`);
